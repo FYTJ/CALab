ad7d636a1e2d8d578ae68832241b4826e92b0bd3
FYTJ <zhuyanbo23@mails.ac.cn>
2025-10-16 15:34:53 +0800

    fix: signed extend condition in WB.v

--- WB.v
+++ WB.v
@@ -34,13 +34,13 @@ module WB (
     // load_op为1时符号扩展，load_op为0时0扩展
     assign mem_result   = 
         {32{load_op[0] | load_op[3]}} &   // LB & LBU
-            ({32{result[1: 0] == 2'b00}} & {{24{load_op[3] & data_sram_rdata[7]}}, data_sram_rdata[7: 0]} | 
-    		{32{result[1: 0] == 2'b01}} & {{24{load_op[3] & data_sram_rdata[15]}}, data_sram_rdata[15: 8]} | 
-			{32{result[1: 0] == 2'b10}} & {{24{load_op[3] & data_sram_rdata[23]}}, data_sram_rdata[23: 16]} | 
-			{32{result[1: 0] == 2'b11}} & {{24{load_op[3] & data_sram_rdata[31]}}, data_sram_rdata[31: 24]}) |
+            ({32{result[1: 0] == 2'b00}} & {{24{load_op[0] & data_sram_rdata[7]}}, data_sram_rdata[7: 0]} | 
+    		{32{result[1: 0] == 2'b01}} & {{24{load_op[0] & data_sram_rdata[15]}}, data_sram_rdata[15: 8]} | 
+			{32{result[1: 0] == 2'b10}} & {{24{load_op[0] & data_sram_rdata[23]}}, data_sram_rdata[23: 16]} | 
+			{32{result[1: 0] == 2'b11}} & {{24{load_op[0] & data_sram_rdata[31]}}, data_sram_rdata[31: 24]}) |
 		{32{load_op[1] | load_op[4]}} &   // LH & LHU
-			({32{result[1: 0] == 2'b00}} & {{16{load_op[4] & data_sram_rdata[15]}}, data_sram_rdata[15: 0]} |
-			{32{result[1: 0] == 2'b10}} & {{16{load_op[4] & data_sram_rdata[31]}}, data_sram_rdata[31: 16]}) |
+			({32{result[1: 0] == 2'b00}} & {{16{load_op[1] & data_sram_rdata[15]}}, data_sram_rdata[15: 0]} |
+			{32{result[1: 0] == 2'b10}} & {{16{load_op[1] & data_sram_rdata[31]}}, data_sram_rdata[31: 16]}) |
 	 	{32{load_op[2]}} & data_sram_rdata;  // LW
     assign final_result = res_from_mem ? mem_result : result;
 
