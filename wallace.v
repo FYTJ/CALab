52a0589f69b0cd4d51c09383ebef101aa7417cb9
FYTJ <zhuyanbo23@mails.ac.cn>
2025-10-21 16:44:22 +0800

    declare input and output as wire explicitly

--- wallace.v
+++ wallace.v
@@ -1,10 +1,10 @@
 module wallace (
-    input [16:0] in,    // 17-bit
-    input [13:0] Cin,
+    input wire [16:0] in,    // 17-bit
+    input wire [13:0] Cin,
 
-    output [13:0] Cout,
-    output S,
-    output C
+    output wire [13:0] Cout,
+    output wire S,
+    output wire C
 );
     // first layer
     wire [4:0] S1;
